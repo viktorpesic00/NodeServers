9e1353e1ebd08e11648fd76b2ef790511319d0e3
Viktor Pesic <viktorpesic00@gmail.com>
2022-04-01 13:18:50 +0200

    auth

--- metAppServer.js
+++ metAppServer.js
@@ -8,6 +8,44 @@ app.use(express.static("./methods-public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.post("/auth", (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(200).json({ success: false, message: "email is empty" });
+  }
+  if (!password) {
+    return res
+      .status(200)
+      .json({ success: false, message: "password is empty" });
+  }
+
+  try {
+    var imap = new Imap({
+      user: email,
+      password: password,
+      host: "mail.metropolitan.ac.rs",
+      port: 993,
+      tls: true,
+    });
+
+    imap.once("error", function (err) {
+      return res
+        .status(200)
+        .json({ success: false, message: "Email or Password is not correct" });
+    });
+    imap.once("ready", function () {
+      return res
+        .status(200)
+        .json({ success: true, message: "Logged in succesfuly" });
+    });
+    imap.connect();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.get("/getPoruka", (req, res) => {
   //  console.log("req");
   let rawdata = fs.readFileSync("poruka.json");
